752b33b6db6a9f08adb864c4e0148a0c1afc4041
munquasim <munquasim3@gmail.com>
2018-05-19 15:06:58 +0400

    setup

--- config/config.js
+++ config/config.js
@@ -1,13 +1,8 @@
 module.exports = {
-    name: "medasAPI",
+    name: "referral-ws",
     env: process.env.NODE_ENV || "development",
     port: process.env.PORT || 3000,
     base_url: process.env.BASE_URL || "http://localhost:3000",
-    db:{
-        host: process.env.DB_HOST || 'localhost',
-        user: process.env.DB_USER || 'root',
-        password : process.env.DB_PSW || 'medteam2013',
-        port : process.env.DB_PORT || 3306, //port mysql
-        database:process.env.DB_NAME || 'eclinic_clinica_new'
-    }
+    HOME:"/Users/talatmahmood/",
+    db: process.env.MONGODB || "mongodb://test:test123@ds219000.mlab.com:19000/test-mobile-setup"
 }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 // export * from './appointment';
 
-var appointment = require('../controllers/appointment');
-var logs = require('../controllers/logs');
+var referPatient = require('../controllers/referPatient');
 var auth = require('../controllers/authenticate');
 var errs = require('restify-errors');
 
@@ -16,62 +15,5 @@ module.exports = function(server){
     });
 
     server.use(auth.isAuthenticate);
-
-    server.get({ path:'/appointment/:id', version:'1.0.0' },(req, res, next)=>{
-        appointment.getById(req.params.id,(err,response) => {
-            if(err) return res.send(400, {DisplayMessage:err});
-            return res.send(200,response);
-        });
-    });
-
-    server.post({ path: '/doc-appoint', version: '1.0.0' },(req, res, next)=>{ 
-        appointment.getByDoctorId(req.body,(err,response) => {
-            if(err) return res.send(400, {DisplayMessage:err});
-            return res.send(200,{data:response.list});
-        });
-    });
-
-    server.post({ path: '/doc-timeslot', version: '1.0.0' }, (req, res, next)=>{
-        appointment.getDoctorSlots(req.body,(err,response) => {
-            if(err) return res.send(400, {DisplayMessage:err});
-            return res.send(200,{data:response});
-        });
-    }); 
-
-    server.get({path:'/getlog', version:'1.0.0'}, (req, res, next)=>{
-        logs.getlog(req.body,(err,response) => {
-            if(err) return res.send(400, {DisplayMessage:err});
-            return res.send(200,{data:response});
-        });
-    });
-
-    /***********VERSION 2.0.0 *****************/
-
-    server.post({ path: '/doc-appoint', version: '2.0.0' },(req, res, next)=>{ 
-        appointment.getByDoctorId(req.body,(err,response) => {
-            if(err) return res.send(400, {DisplayMessage:err});
-            return res.send(200,{data:response});
-        });
-    });
-
-    server.post({ path: '/doc-timeslot', version: '2.0.0' }, (req, res, next)=>{ 
-        appointment.getDoctorSlots(req.body,(err,response) => {
-            if(err) return res.send(400, {DisplayMessage:err});
-            return res.send(200,{data:response});
-        });
-    });
     
-    server.post({ path: '/week-appoint', version: '2.0.0' },(req, res, next)=>{ 
-        appointment.getWeeklyAppointment(req.body,(err,response) => {
-            if(err) return res.send(400, {DisplayMessage:err});
-            return res.send(200,{data:response});
-        });
-    });
-
-    server.post({ path: '/month-appoint', version: '2.0.0' },(req, res, next)=>{ 
-        appointment.getMonthlyAppointment(req.body,(err,response) => {
-            if(err) return res.send(400, {DisplayMessage:err});
-            return res.send(200,{data:response});
-        });
-    });
 }
